52e403e4f306df2cae26c0ebd2e7f073ff08fa96
David Peluffo <peluffodavid@gmail.com>
2024-10-16 20:30:12 -0500

    vista administrador actualizada
    diamante.jpg  reducir imagen px

--- administrador.php
+++ administrador.php
@@ -1,31 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+
 <head>
+
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administradoor</title>
-    <link rel="stylesheet" href="css/administrador.css">
-    <link rel="icon" href="img/LOGO.jpeg" type="image/png">
+    <title>Administrador</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-</head>
+    <link rel="icon" href="img/LOGO.jpeg">
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+   />
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    
+</head><style>
+        body {
+    
+    background: url(img/diamante.jpg);
+    background-repeat: no-repeat;
+    background-size: cover; /* O usa 'contain' si prefieres que la imagen esté completamente visible sin recortes */
+    background-position: center center; /* Centra la imagen */
+     /* Hace que la imagen permanezca fija al hacer scroll */
+    height: 80vh; /* Asegura que el fondo cubra toda la altura de la ventana */
+    margin: 0; /* Elimina cualquier margen por defecto */
+    padding: 0; /* Elimina cualquier padding por defecto */
+    display: flex;
+    justify-content: center; /* Centra horizontalmente */
+    align-items: center; /* Centra verticalmente */
+}
+    </style>
+
 
 <body>
+
+
+  <nav class="navbar-second flex">
+    <div class="logo">
+    <img class="logonav" src="img/1-2.png" width="200" alt="Logo de la página web">
+</div>
+      <ul class="flex menu">
+        <li><a href="servicios.php"><i class='bx bx-wrench'></i> Buscar clientes</a></li>
+        <li><a href="configuracion.php"><i class='bx bx-cog'></i> Configuración</a></li>
+        <li><a href="crearusuario.php"><i class='bx bx-user'></i> Crear Usuarios</a></li>
+        <li><a href="citasahendadas.php"><i class='bx bx-user'></i> Citas  </a></li>
+        <!-- <li><a href= "#productos">Usarios</a></li>  -->
+        
+</ul>
+<!-- </nav>
+
     <div class="menu-button-container">
-        <button id="show-options-button" onclick="toggleOptions()">
-            <i class="fas fa-bars"></i> Menú
-        </button>
+    <button id="show-options-button" onclick="toggleOptions()"><i class='bx bx-menu'></i> Menú</button>
     </div>
     <div class="options" id="options">
-        <a href="buscar_clientes.php">
-            <i class="fas fa-search"></i> Buscar clientes
-        </a>
-        <a href="crear_usuario.php">
-            <i class="fas fa-user-plus"></i> Crear Usuarios
-        </a>
-        <a href="ver_citas.php">
-            <i class="fas fa-calendar-check"></i> Ver citas agendadas
-        </a>
+        
+        <a href="servicios.php"><i class='bx bx-wrench'></i> Buscar clientes</a>
+        <a href="configuracion.php"><i class='bx bx-cog'></i> Configuración</a>
+        <a href="crearusuario.php"><i class='bx bx-user'></i> Crear Usuarios</a>
     </div>
     <script src="js/administrador.js"></script>
-    <script src="js/inactividad.js"></script>
+     <script src="js/inactividad.js"></script> --> 
 </body>
 
 </html>
