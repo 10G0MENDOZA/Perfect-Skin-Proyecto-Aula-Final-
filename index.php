60535da9f5f6cb26ff2448147936a773122959a9
gersonjim <gersonjim1222@gmail.com>
2024-10-18 02:02:44 -0500

    todos los css estan por su section

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Perfect-Skin</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/acercanosotros.css">
+    <link rel="stylesheet" href="css/indexservicios.css">
+    <link rel="stylesheet" href="css/indexproductos.css">
+    <link rel="stylesheet" href="css/porqueperfectskin.css">
+    <link rel="stylesheet" href="css/horariodeatencion.css">
+    <link rel="stylesheet" href="css/indextestimonios.css">
     <link rel="icon" href="img/LOGO.jpeg">
 
 
@@ -300,120 +307,108 @@
 
 
              <div id="servicios" class="servicios-section">
-                        <div class="header-productos">
-                         <h2>Productos</h2>
-                      </div>  
-                    <div class="swiper mySwiperServicios">
-                        <div class="swiper-wrapper">
+    <div class="header-productos">
+        <h2>Productos</h2>
+    </div>  
+    <div class="swiper mySwiperServicios">
+        <div class="swiper-wrapper">
 
-                            <!-- Servicio 1 -->
-                            <div class="swiper-slide">
-                                <div class="servicio">
-                                    <div class="servicio-imagen">
-                                        <img src="img/protectorsolar.jpg" alt="Servicio 1">
-                                    </div>
-                                    <div class="servicio-descripcion">
-                                        <h3>Protector Solar</h3>
-                                        <p>Relájate y protege tu piel con nuestro servicio de protector solar
-                                            personalizado.</p>
-                                        <span class="precio-servicio">$41.500 COP</span>
-                                        <a href="producto.php" class="btn-servicio">ver más</a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Servicio 2 -->
-                            <div class="swiper-slide">
-                                <div class="servicio">
-                                    <div class="servicio-imagen">
-                                        <img src="img/antiarrugas.png" alt="Servicio 2">
-                                    </div>
-                                    <div class="servicio-descripcion">
-                                        <h3>Antiarrugas</h3>
-                                        <p>Mejora la apariencia de tu piel con nuestro tratamiento especializado
-                                            antiedad.</p>
-                                        <span class="precio-servicio">$80.000 COP</span>
-                                        <a href="producto.php" class="btn-servicio">ver más</a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Servicio 3 -->
-                            <div class="swiper-slide">
-                                <div class="servicio">
-                                    <div class="servicio-imagen">
-                                        <img src="img/aguamiselar.jpg" alt="Servicio 3">
-                                    </div>
-                                    <div class="servicio-descripcion">
-                                        <h3>Agua micelar</h3>
-                                        <p>Agua micelar con acción 7 en 1 que deja una piel limpia, desmaquillada,
-                                            fresca e hidratada respetando el pH de la piel sin dañar su barrera natur
-                                        </p>
-                                        <span class="precio-servicio">$40.000 COP</span>
-                                        <a href="producto.php" class="btn-servicio">ver más</a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Servicio 4 -->
-                            <div class="swiper-slide">
-                                <div class="servicio">
-                                    <div class="servicio-imagen">
-                                        <img src="img/hydrobost.jpg" alt="Servicio 4">
-                                    </div>
-                                    <div class="servicio-descripcion">
-                                        <h3>Serum Hidratante</h3>
-                                        <p>Serum hidratante concentrado para cara y ojos que mantiene la piel suave,
-                                            fortalecida e hidratada gracias a su formulación con 2 densidades de Ácido
-                                            Hialurónico, Pro-Vitamina B5 y Glicerina.</p>
-                                        <span class="precio-servicio">$70.000 COP</span>
-                                        <a href="producto.php" class="btn-servicio">ver más</a>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div class="swiper-slide">
-                                <div class="servicio">
-                                    <div class="servicio-imagen">
-                                        <img src="img/gellimpiador.jpg" alt="Servicio 5">
-                                    </div>
-                                    <div class="servicio-descripcion">
-                                        <h3>Gel Limpiador Facial</h3>
-                                        <p>Limpiador en gel que limpia suave y profundamente mientras aumenta la
-                                            hidratación,
-                                            dejando la piel con una sensación refrescante, limpia y suave.</p>
-                                        <span class="precio-servicio">$35.000 COP</span>
-                                        <a href="producto.php" class="btn-servicio">ver más</a>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                            <div class="swiper-slide">
-                                <div class="servicio">
-                                    <div class="servicio-imagen">
-                                        <img src="img/desmaquillante.jpg" alt="Servicio 6">
-                                    </div>
-                                    <div class="servicio-descripcion">
-                                        <h3>Toallitas desmaquillantes faciales</h3>
-                                        <p>Toallitas desmaquillantes suaves y delicadas con aroma relajante para la
-                                            noche que remueven 99% del maquillaje de manera suave
-                                            y efectiva en un solo paso, dejando la piel completamente limpia de
-                                            residuos.</p>
-                                        <span class="precio-servicio">$100.000 COP</span>
-                                        <a href="producto.php" class="btn-servicio">ver más</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                        <div class="swiper-button-next servicios-button-next"></div>
-                        <div class="swiper-button-prev servicios-button-prev"></div>
+            <!-- Servicio 1 -->
+            <div class="swiper-slide"> <!-- Cambiado de swiper-slide1 a swiper-slide -->
+                <div class="servicio">
+                    <div class="servicio-imagen">
+                        <img src="img/protectorsolar.jpg" alt="Servicio 1">
                     </div>
-               </div> 
+                    <div class="servicio-descripcion">
+                        <h3>Protector Solar</h3>
+                        <p>Relájate y protege tu piel con nuestro servicio de protector solar personalizado.</p>
+                        <span class="precio-servicio">$41.500 COP</span>
+                        <a href="producto.php" class="btn-servicio">ver más</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Servicio 2 -->
+            <div class="swiper-slide">
+                <div class="servicio">
+                    <div class="servicio-imagen">
+                        <img src="img/antiarrugas.png" alt="Servicio 2">
+                    </div>
+                    <div class="servicio-descripcion">
+                        <h3>Antiarrugas</h3>
+                        <p>Mejora la apariencia de tu piel con nuestro tratamiento especializado antiedad.</p>
+                        <span class="precio-servicio">$80.000 COP</span>
+                        <a href="producto.php" class="btn-servicio">ver más</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Servicio 3 -->
+            <div class="swiper-slide">
+                <div class="servicio">
+                    <div class="servicio-imagen">
+                        <img src="img/aguamiselar.jpg" alt="Servicio 3">
+                    </div>
+                    <div class="servicio-descripcion">
+                        <h3>Agua micelar</h3>
+                        <p>Agua micelar con acción 7 en 1 que deja una piel limpia, desmaquillada, fresca e hidratada respetando el pH de la piel sin dañar su barrera natural.</p>
+                        <span class="precio-servicio">$40.000 COP</span>
+                        <a href="producto.php" class="btn-servicio">ver más</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Servicio 4 -->
+            <div class="swiper-slide">
+                <div class="servicio">
+                    <div class="servicio-imagen">
+                        <img src="img/hydrobost.jpg" alt="Servicio 4">
+                    </div>
+                    <div class="servicio-descripcion">
+                        <h3>Serum Hidratante</h3>
+                        <p>Serum hidratante concentrado para cara y ojos que mantiene la piel suave, fortalecida e hidratada gracias a su formulación con 2 densidades de Ácido Hialurónico, Pro-Vitamina B5 y Glicerina.</p>
+                        <span class="precio-servicio">$70.000 COP</span>
+                        <a href="producto.php" class="btn-servicio">ver más</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Servicio 5 -->
+            <div class="swiper-slide">
+                <div class="servicio">
+                    <div class="servicio-imagen">
+                        <img src="img/gellimpiador.jpg" alt="Servicio 5">
+                    </div>
+                    <div class="servicio-descripcion">
+                        <h3>Gel Limpiador Facial</h3>
+                        <p>Limpiador en gel que limpia suave y profundamente mientras aumenta la hidratación, dejando la piel con una sensación refrescante, limpia y suave.</p>
+                        <span class="precio-servicio">$35.000 COP</span>
+                        <a href="producto.php" class="btn-servicio">ver más</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Servicio 6 -->
+            <div class="swiper-slide">
+                <div class="servicio">
+                    <div class="servicio-imagen">
+                        <img src="img/desmaquillante.jpg" alt="Servicio 6">
+                    </div>
+                    <div class="servicio-descripcion">
+                        <h3>Toallitas desmaquillantes faciales</h3>
+                        <p>Toallitas desmaquillantes suaves y delicadas con aroma relajante para la noche que remueven 99% del maquillaje de manera suave y efectiva en un solo paso, dejando la piel completamente limpia de residuos.</p>
+                        <span class="precio-servicio">$100.000 COP</span>
+                        <a href="producto.php" class="btn-servicio">ver más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Botones de navegación -->
+        <div class="swiper-button-next servicios-button-next"></div>
+        <div class="swiper-button-prev servicios-button-prev"></div>
+    </div>
+</div>
 
                <div class="porque-perfectskin-section">
     <div class="content-container">
